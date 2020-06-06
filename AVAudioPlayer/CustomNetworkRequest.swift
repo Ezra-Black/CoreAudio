15681bde1256a6c51221ce85eaf50e6fd483158a
Ezra Black <dev.ezrablack@gmail.com>
2020-06-06 12:51:44 -0500

    updates

---
+++ AVAudioPlayer/CustomNetworkRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CustomNetworkRequest.swift
+//  AVAudioPlayer
+//
+//  Created by Ezra Black on 6/6/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+
+struct CustomNetworkRequest {
+    let url: URL
+}
